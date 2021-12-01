a938b17f556d6e31c38579459766e259363d2967
wangpingan <pingan8787@qq.com>
2021-12-01 08:22:27 +0800

    feat: update v-model

---
+++ Vue2/src/components/v-model-child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    我是子组件: 
+    <input type="text" :value="value" @input="$emit('input', $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'child',
+  props: ['value'],
+  methods: {
+    onChange ($event) {
+      this.$emit('change', $event.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+.child{
+  padding-left: 10px;
+}
+</style>
---
+++ Vue2/src/views/api/v-model.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>v-model 参数</h1>
+    <v-model-child v-model="title"></v-model-child>
+    <!-- 
+        等价于
+        <custom-input
+            v-bind:value="title"
+            v-on:input="title = $event"
+        ></custom-input>
+     -->
+    <div>父组件的值：{{title}}</div>
+  </div>
+</template>
+<script>
+/**
+ * 一个组件上的 v-model 默认会利用名为 value 的 prop 和名为 input 的事件，但是像单选框、复选框等类型的输入控件可能会将 value attribute 用于不同的目的。
+ * v-model 等同于 :value="title" @input="title=$event.target.value"
+ * 文档地址：https://cn.vuejs.org/v2/guide/components-custom-events.html
+ */
+import vModelChild from '../../components/v-model-child.vue'
+
+export default {
+  components: { vModelChild },
+  data() {
+    return {
+      title: 'hello leo',
+    };
+  },
+  methods: {
+      onChange(val){
+          this.title = val;
+      }
+  },
+};
+</script>
